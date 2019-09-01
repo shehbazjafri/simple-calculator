0677642579d1a738cc3a531205b2258946c44c43
shehbazjafri <galaxytwirls@gmail.com>
2019-09-01 23:53:45 +0530

    Updates

---
+++ precache-manifest.62780a9ba4be538c3e485d07e60ec0c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086d43f0591c33928e821aad08b23ae5",
+    "url": "/simple-calculator/index.html"
+  },
+  {
+    "revision": "8dcf8c7986a83656e115",
+    "url": "/simple-calculator/static/css/main.24ad3a0c.chunk.css"
+  },
+  {
+    "revision": "af1b6b08e2a705d0b8e7",
+    "url": "/simple-calculator/static/js/2.7b0e938e.chunk.js"
+  },
+  {
+    "revision": "8dcf8c7986a83656e115",
+    "url": "/simple-calculator/static/js/main.286c489a.chunk.js"
+  },
+  {
+    "revision": "624ded25012065399b97",
+    "url": "/simple-calculator/static/js/runtime~main.d489fee8.js"
+  }
+]);
